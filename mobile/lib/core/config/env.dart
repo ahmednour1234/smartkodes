3e809feabe979345fac7bdb9ca78f0aa586b466d
Amgad Ezzat <megostar59@gmail.com>
2026-02-16 15:39:20 +0200

    Update API base URL in environment configuration for production deployment

--- mobile/lib/core/config/env.dart
+++ mobile/lib/core/config/env.dart
@@ -2,6 +2,6 @@ class Env {
   Env._();
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'https://5bfe-156-203-155-96.ngrok-free.app/api/v1/',
+    defaultValue: 'http://smartcodes.syscomdemos.com/api/v1/',
   );
 }
