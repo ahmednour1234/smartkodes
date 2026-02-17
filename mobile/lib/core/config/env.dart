fa50a1a6ae58390f2cc43dcbd46ba0a92532b5ca
Amgad Ezzat <megostar59@gmail.com>
2026-02-17 13:05:52 +0200

    Update API base URL in environment configuration for consistency

--- mobile/lib/core/config/env.dart
+++ mobile/lib/core/config/env.dart
@@ -2,6 +2,6 @@ class Env {
   Env._();
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://smartcodes.syscomdemos.com/api/v1/',
+    defaultValue: 'http://smartkodes.syscomdemos.com/api/v1/',
   );
 }
