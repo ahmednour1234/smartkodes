7e7ff768554494affbf6f07f26cab9e815f3d678
Amgad Ezzat <megostar59@gmail.com>
2026-04-26 19:26:03 +0300

    update env mobile

--- mobile/lib/core/config/env.dart
+++ mobile/lib/core/config/env.dart
@@ -3,11 +3,11 @@ class Env {
 
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'https://smartkodes.syscomdemos.com/api/v1/',
+    defaultValue: 'https://ssite.app/api/v1/',
   );
 
   static const String webBaseUrl = String.fromEnvironment(
     'WEB_BASE_URL',
-    defaultValue: '',
+    defaultValue: 'https://ssite.app/',
   );
 }
